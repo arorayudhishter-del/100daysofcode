46f005f305668c11595a3d27aafbdc81aa5112fc
arorayudhishter@gmail.com <arorayudhishter@gmail.com>
2026-08-21 15:04:50 +0530

    Changed the buggy code

--- Day1_code.c
+++ Day1_code.c
@@ -3,8 +3,10 @@ int main()
 {
     int a,b;
     printf("Enter 2 numbers here:");
-    scanf("%d %d", &a, &b);
-    int sum = a = b;
+    scanf("%d", &a);
+    printf("Enter  numbers here:");
+    scanf("%d", &b);
+    int sum = a + b;
     printf("The sum of the numbers is %d", sum);
     return 0;
 }
