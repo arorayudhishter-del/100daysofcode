636332a0907094218fd4d83e0f0a4bad4072a650
arorayudhishter@gmail.com <arorayudhishter@gmail.com>
2026-08-21 15:10:42 +0530

    Fixing small mistakes

--- Day2_code.c
+++ Day2_code.c
@@ -2,15 +2,18 @@
 int main()
 {
     int a,b;
-    printf("Enter 2 numbers here:");
-    scanf("%d %d", &a, &b);
     int sum = a = b;
     int product = a * b;
     int subtract = a - b;
     int quetiont = a % b;
+    printf("Enter a number here:");
+    scanf("%d", &a);
+    printf("Enter a number here:");
+    scanf("%d", &b);
+    
     printf("The sum of the numbers is %d", sum);
-    printf("The product of the numbers is %d", product);
-    printf("The diffrence of the numbers is %d", subtract);
+    printf("\nThe product of the numbers is %d", product);
+    printf("\nThe diffrence of the numbers is %d", subtract);
     printf("The quetiont of the numbers is %d", quetiont);
     return 0;
 }
