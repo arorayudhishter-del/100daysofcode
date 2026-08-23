4ff7dfdeb608c5cbca7ee73b2dc67c16aa150f58
arorayudhishter@gmail.com <arorayudhishter@gmail.com>
2026-08-24 00:57:19 +0530

    2nd question of the day 13

---
+++ Day13_code2.c
@@ -0,0 +1,14 @@
+#include <Stdio.h>
+int main()
+{
+    int n;
+    printf("Entere a numbere here:");
+    scanf("%d", &n);
+    for(int i = 0; i <= n; i++)
+    {
+        
+        printf("\nnum: %d", i);
+
+    }
+    return 0;
+}
