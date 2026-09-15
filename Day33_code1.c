3d807eefba37760152483242286ff427585e0b24
arorayudhishter@gmail.com <arorayudhishter@gmail.com>
2026-09-15 18:12:20 +0530

    Search a random element in the given array

---
+++ Day33_code1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 45, 56, 72};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key, low = 0, high = n - 1, mid;
+    int found = 0;
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            printf("Element %d found at position %d\n", key, mid + 1);
+            found = 1;
+            break;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (!found) {
+        printf("Element %d not found in the array\n", key);
+    }
+
+    return 0;
+}
