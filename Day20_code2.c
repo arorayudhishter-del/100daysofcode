aeb6be1eceab489fc8a331da2daf66a61ff8ec6e
Yudi🌟 <arorayudhishter@gmail.com>
2026-09-15 19:32:42 +0530

    Add files via upload
    Sum of the odd numbers

---
+++ Day20_code2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char binary[100];
+    int i;
+
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    printf("1's complement = ");
+    for (i = 0; i < strlen(binary); i++) {
+        if (binary[i] == '0')
+            printf("1");
+        else if (binary[i] == '1')
+            printf("0");
+        else {
+            printf("\nInvalid input! Only 0s and 1s are allowed.\n");
+            return 1;
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
