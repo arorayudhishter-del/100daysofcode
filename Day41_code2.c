75c9b4b70fa44731cb4aefe327664c377545d894
Yudi🌟 <arorayudhishter@gmail.com>
2026-09-22 15:14:10 +0530

    Add files via upload
    Find the transpose of the matrix

---
+++ Day41_code2.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main() {
+    int a[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int transpose[3][3];
+    int i, j;
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
