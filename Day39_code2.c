8c35c6fffe609045247713719b654c5f965c41e5
Yudi🌟 <arorayudhishter@gmail.com>
2026-09-22 14:54:51 +0530

    Add files via upload
    Find the sum of the main diagonal element of the super matrix

---
+++ Day39_code2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int i, sum = 0;
+
+    for(i = 0; i < 3; i++) {
+        sum += a[i][i];
+    }
+
+    printf("Sum of main diagonal elements = %d\n", sum);
+
+    return 0;
+}
